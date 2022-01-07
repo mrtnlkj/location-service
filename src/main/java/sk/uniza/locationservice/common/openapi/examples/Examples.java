e1d3744a0a9acb4c68a4cfbd802c505d2c104a54
Martin Lokaj <mrtn.lkj@gmail.com>
2022-01-07 23:58:16 +0100

    added openapi definition example

---
+++ src/main/java/sk/uniza/locationservice/common/openapi/examples/Examples.java
@@ -0,0 +1,6 @@
+package sk.uniza.locationservice.common.openapi.examples;
+
+public class Examples {
+	public static final String EXAMPLE_RESPONSE = "{\"data\":\"data\"}";
+
+}
--- src/main/java/sk/uniza/locationservice/controller/ExampleController.java
+++ src/main/java/sk/uniza/locationservice/controller/ExampleController.java
@@ -1,5 +1,12 @@
 package sk.uniza.locationservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -9,17 +16,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import sk.uniza.locationservice.bean.ExampleResponse;
+import sk.uniza.locationservice.common.openapi.examples.ErrorExamples;
+import sk.uniza.locationservice.common.openapi.examples.Examples;
 import sk.uniza.locationservice.service.ExampleService;
 
 @Slf4j
 @RestController
 @RequestMapping("/examples")
 @RequiredArgsConstructor
+@Tag(name = ExampleController.API_TAG, description = ExampleController.API_DESCRIPTION)
 public class ExampleController {
+
+	public final static String API_TAG = "ExampleController";
+	public final static String API_DESCRIPTION = "Description of example controller.";
 
 	private final ExampleService exampleService;
 
 	@GetMapping("/{name}")
+	@Operation(
+			summary = "Summary.",
+			description = "Description."
+	)
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Success.",
+					content = @Content(schema = @Schema(implementation = ExampleResponse.class), examples = {
+							@ExampleObject(name = "ExampleResponse", value = Examples.EXAMPLE_RESPONSE),
+					})),
+			@ApiResponse(responseCode = "500", description = "Internal Server Error.",
+					content = @Content(schema = @Schema(implementation = String.class), examples = {
+							@ExampleObject(name = "Error", value = ErrorExamples.ERROR, description = "Unexpected error occurred."),
+					}))
+	})
 	public ResponseEntity<?> getExample(@PathVariable(name = "name") String name) {
 		ExampleResponse response = exampleService.getExample(name);
 		return ResponseEntity.ok().body(response);
