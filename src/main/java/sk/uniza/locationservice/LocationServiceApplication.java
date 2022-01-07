e89707449e42dc2ea3e1d37baef0218e149c0811
Martin Lokaj <mrtn.lkj@gmail.com>
2022-01-07 21:07:29 +0100

    init default spring boot project

---
+++ src/main/java/sk/uniza/locationservice/LocationServiceApplication.java
@@ -0,0 +1,13 @@
+package sk.uniza.locationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationServiceApplication.class, args);
+	}
+
+}
