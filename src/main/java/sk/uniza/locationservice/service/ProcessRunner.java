8aa99a834704765ae88563298826096c6cba2c12
Martin Lokaj <mrtn.lkj@gmail.com>
2022-01-08 21:40:50 +0100

    abstract process runner implemented

---
+++ src/main/java/sk/uniza/locationservice/service/ProcessRunner.java
@@ -0,0 +1,28 @@
+package sk.uniza.locationservice.service;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Slf4j
+public abstract class ProcessRunner {
+
+	protected abstract Map<String, String> getEnvironmentProperties();
+
+	public int runCommand(String[] cmd) throws IOException, InterruptedException {
+		log.info("Process with command \"{}\" started.", (Object) cmd);
+		ProcessBuilder processBuilder = new ProcessBuilder().inheritIO();
+		if (getEnvironmentProperties() != null) {
+			processBuilder.environment().putAll(getEnvironmentProperties());
+		}
+		processBuilder.command(cmd);
+		Process process = processBuilder.start();
+		int exitCode = process.waitFor();
+		log.info("Process finished with exit code: {}.", exitCode);
+		return exitCode;
+	}
+
+}
